602c286592f76565b560cd027156bbedc9b9cfcf
Igor <spirit-drive@yandex.ru>
2022-03-08 22:43:27 +0300

    use link

--- src/screens/NotFound/NotFound.tsx
+++ src/screens/NotFound/NotFound.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Frame } from 'src/components/Frame';
+import { Link } from 'react-router-dom';
 import s from './NotFound.module.sass';
 
 const NotFound: React.FC = () => (
   <div className={s.root}>
-    <Frame>404</Frame>
+    <Frame>
+      <div>404</div>
+      <Link to=".">to home</Link>
+    </Frame>
   </div>
 );
 
