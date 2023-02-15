c44206b74fab0100a567220dd7a9c6316f2ef25a
Igor <spirit-drive@yandex.ru>
2023-02-15 18:39:18 +0300

    refactor(src/navigation/ProtectedRoute): improve code

--- src/navigation/ProtectedRoute.tsx
+++ src/navigation/ProtectedRoute.tsx
@@ -1,10 +1,10 @@
-import React, { ReactElement, FC } from 'react';
+import React, { FC } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useTokenContext } from 'src/TokenProvider';
 
 export const ProtectedRoute: FC = ({ children }) => {
   const [token] = useTokenContext();
   const location = useLocation();
-  if (token) return React.Children.only(children as ReactElement);
+  if (token) return <>{children}</>;
   return <Navigate to="/auth" state={{ from: location }} replace />;
 };
